5c75e0fe678f53b9a379d6d2a3bcbfc6ab942abd
marek.soltis <marek.soltis@wirecard.com>
2018-10-31 11:13:33 +0100

    Numbers Uloha

---
+++ number/uloha/Main.java
@@ -0,0 +1,29 @@
+package number.uloha;
+
+public class Main {
+
+  public static void metoda1(Number vstup){
+
+    if (vstup instanceof Integer) {
+      System.out.println("integer");
+      int i = vstup.intValue();
+      System.out.println(i*10);
+    }
+    else{
+      System.out.println("double");
+      double d = vstup.doubleValue();
+      System.out.println(d*10);
+    }
+
+  }
+
+  public static void main(String[] args) {
+
+    metoda1(10);
+    metoda1(2.5);
+    metoda1(15);
+
+  }
+
+  }
+
