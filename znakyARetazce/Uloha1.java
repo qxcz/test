69fc72356b3775d27e03b798ddc1a8fb3c6c9b07
marek.soltis <marek.soltis@wirecard.com>
2018-11-02 15:40:48 +0100

    znakyRetazce uloha1

--- znakyARetazce/Uloha1.java
+++ znakyARetazce/Uloha1.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
 
 public class Uloha1 {
 
-
-  public static void transformToCapital() {
-
-
+  public static String readFromKeyboard(){
     Scanner vstup = new Scanner(System.in);
 
     System.out.println("Zadajte text: ");
     String s = vstup.nextLine();
-    char textParse[] = s.toCharArray();
+    return s;
+  }
 
+
+  public static void transformToCapital() {
+
+    String s = readFromKeyboard();
+    char textParse[] = s.toCharArray();
 
     for (int i=0; i < s.length(); i++ )
     { if (i< s.length()-1)
@@ -29,12 +32,35 @@ public class Uloha1 {
     String s2 = String.copyValueOf(textParse);
     System.out.println(s2);
 
+  }
+
+  public static void transformToAt() {
+
+    String s = readFromKeyboard();
+    char textParse[] = s.toCharArray();
+
+    for (int i=0; i < s.length(); i++ )
+    {
+      if ( textParse[i] != ' ') {textParse[i]= '@';}
+    }
+    String s2 = String.copyValueOf(textParse);
+    System.out.println(s2);
+
+  }
+
+  public static void cutLastComma(){
+
+    String s = readFromKeyboard();
+    System.out.println(s.substring(0,s.lastIndexOf(",")));
+
 
   }
 
   public static void main(String[] args) {
 
-    transformToCapital();
+    //transformToCapital();
+    //transformToAt();
+    cutLastComma();
 
   }
 
